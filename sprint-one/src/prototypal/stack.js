ba69bdcb16ecb2cfa017231c176435a2191c2d20
James Byrd <jd3yrd@gmail.com>
2017-11-15 16:30:00 -0600

    Prototype stack methods working

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,16 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.stack = {};
+  someInstance.count = 0;
+  console.log(someInstance);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size :  function(){return this.count;},
+};
 
 
